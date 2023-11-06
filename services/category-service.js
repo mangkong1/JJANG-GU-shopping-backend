291c9c3a00a7cce611f8650c33d1d953dc3cf20d
Normaljun95 <jsj950629@gmail.com>
2023-11-06 20:54:30 +0900

    ✨Feat : first category-service

---
+++ services/category-service.js
@@ -0,0 +1,65 @@
+import { categoryModel } from "../db";
+
+class CategoryService {
+    constructor(categoryModel) {
+        this.categoryModel = categoryModel;
+    }
+
+    // 카테고리 생성
+    async addCategory(categoryInfo) {
+        const { name } = categoryInfo;
+        const category = await this.categoryModel.findByName(name);
+
+        if (category) {
+            throw new Error('해당 카테고리명은 현재 사용 중입니다. 다른 카테고리명을 입력해주세요.');
+        }
+
+        const createdNewCategory = await this.categoryModel.create(categoryInfo);
+        return createdNewCategory;
+    }
+
+    // 카테고리 조회
+    async getCategories() {
+        const categories = await this.categoryModel.findAll();
+        return categories;
+    }
+
+    async getCategory(categoryId) {
+        const category = await this.categoryModel.findById(categoryId);
+
+        if (!category) {
+            throw new Error('카테고리를 찾을 수 없습니다.');
+        }
+
+        return category;
+    }
+
+    // 카테고리 업데이트
+    async updateCategory(categoryId, updateData) {
+        const category = await this.categoryModel.findById(categoryId);
+    
+        if (!category) {
+            throw new Error('카테고리를 찾을 수 없습니다.');
+        }
+    
+        const updatedCategory = await this.categoryModel.update(categoryId, updateData);
+    
+        return updatedCategory;
+    }
+
+    // 카테고리 제거
+    async remove(categoryId) {
+        const category = await this.categoryModel.findById(categoryId);
+    
+        if (!category) {
+          throw new Error('등록된 카테고리가 없습니다. 다시 한 번 확인해 주세요.');
+        }
+    
+        category = await this.categoryModel.delete(categoryId);
+        return category;
+    }
+}
+
+const categoryService = new CategoryService(categoryModel);
+
+export { categoryService };
