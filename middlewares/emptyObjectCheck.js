e4b33ab3454e554d48ca1b22170b2a97c9eba784
Normaljun95 <jsj950629@gmail.com>
2023-11-10 00:32:54 +0900

    🐛Fix : change empty object check

--- middlewares/emptyObjectCheck.js
+++ middlewares/emptyObjectCheck.js
@@ -1,12 +1,7 @@
-import * as Joi from "joi";
+import is from '@sindresorhus/is';
 
 function emptyObejctCheck(req, res, next) {
-  // 유효성 검사 스키마 정의
-  const schema = Joi.object().keys({});
-
-  const { error } = schema.validate(req.body);
-
-  if (error) {
+  if (is.emptyObject(req.body)) {
     res.status(400).json({
       result: 'Content-Type',
       reason: 'headers의 Content-Type을 application/json으로 설정해주세요',
